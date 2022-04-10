9da142bf8e0d70a2567ba7f46124b27e0b31f86b
jayson7 <lexxiijoo70@gmail.com>
2022-04-10 04:47:26 +0100

    basic config

---
+++ apps/serializers.py
@@ -0,0 +1,14 @@
+"""model serializers"""
+
+from rest_framework import serializers
+from .models import Post
+
+class NoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        read_only_fields = (
+            "created_at",
+            "updated_at",
+        )
+        fields = '__all__'
